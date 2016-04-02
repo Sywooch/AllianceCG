fd1292a9cf4ce9c24b6e0b3fa8f198451d5160f6
Maxim Ishchenko <maxim.ishchenko@gmail.com>
2016-04-02 16:15:35 +0300

    Skoda Sidebar. Added Count Label

--- modules/skoda/views/layouts/_sidebar.php
+++ modules/skoda/views/layouts/_sidebar.php
@@ -4,6 +4,9 @@ use yii\bootstrap\Nav;
 use yii\helpers\Html;
 use yii\helpers\Url;
 use app\modules\skoda\Module;
+use rmrevin\yii\fontawesome\FA;
+
+$count = '1';
 
     echo Nav::widget([
         'options' => ['class' => 'nav nav-pills nav-stacked'],
@@ -18,7 +21,7 @@ use app\modules\skoda\Module;
                 'url' => '/skoda/servicesheduler/calendar',
             ],
             [
-                'label' => Module::t('module', 'STATUS_TITLE'),
+                'label' => Module::t('module', 'STATUS_TITLE') . ' <span class="label label-pill label-success">' . FA::icon('calculator') . ' ' . $count . '</span>',
                 'url' => '/skoda/statusmonitor/',
             ],
         ]),

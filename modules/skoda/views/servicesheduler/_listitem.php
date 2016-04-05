1559f67fb62a13944b0e5c91ed77ebe5597a6e2a
Maxim Ishchenko <maxim.ishchenko@gmail.com>
2016-04-05 23:39:44 +0300

    Skoda Servicesheduler. Added DetailView to ListView

--- modules/skoda/views/servicesheduler/_listitem.php
+++ modules/skoda/views/servicesheduler/_listitem.php
@@ -9,15 +9,26 @@
     use yii\helpers\HtmlPurifier;
     use app\modules\user\models\User;
     use rmrevin\yii\fontawesome\FA;
+    use yii\widgets\DetailView;
 ?>
 
 <?php 
     $model->date = Yii::$app->formatter->asDate($model->date , 'dd/MM/yyyy');
 ?>
 
-    <div class="col-lg-12">
-        <?= '<h4>' . FA::icon('user') . ' ' . HtmlPurifier::process($model->responsible) .'</h4>' ?>
-    </div>  
-    <div class="col-lg-12">
-        <?= '<h5>' . FA::icon('calendar') . ' ' . HtmlPurifier::process($model->date) .'</h5>' ?>
-    </div>    
+<?php
+echo DetailView::widget([
+    'model' => $model,
+    'attributes' => [
+        'responsible',
+        'date',
+    ],
+]);
+?>
+
+<!--    <div class="col-lg-12">
+        <div class="servicesheduler-list">-->
+            <?php '<h4>' . FA::icon('user') . ' ' . $model->getAttributeLabel('responsible') . ' ' . HtmlPurifier::process($model->responsible) .'</h4>' ?>
+            <?php '<h5>' . FA::icon('calendar') . ' ' . HtmlPurifier::process($model->date) .'</h5>' ?>
+<!--        </div>
+    </div>-->

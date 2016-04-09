323472a2175fd70da45927c724f1032d6f8cabbd
Maxim Ishchenko <maxim.ishchenko@gmail.com>
2016-04-09 16:45:39 +0300

    Alliance Phonebook. Fixed Single File Phonebook

--- modules/alliance/views/phonebook/index.php
+++ modules/alliance/views/phonebook/index.php
@@ -12,14 +12,12 @@ $this->title = Module::t('module', 'NAV_ALLIANCE_PHONEBOOK');
 $this->params['breadcrumbs'][] = ['label' => Module::t('module', 'NAV_ALLIANCE'), 'url' => ['/alliance']];
 $this->params['breadcrumbs'][] = $this->title;    
 
-$ldaphost = "10.18.123.17";         // Ваш сервер ldap
-$ldapport = 389;                    // Порт вашего сервера ldap
+$ldaphost = "10.18.123.17";
+$ldapport = 389;
 
 // Соединение с LDAP
 $ds = ldap_connect($ldaphost, $ldapport)
           or die("Невозможно соединиться с $ldaphost");
-
-//$ds=ldap_connect("10.18.123.17");
 
 if ($ds) {
     
@@ -27,15 +25,13 @@ if ($ds) {
     
     // Анонимная привязка, доступ только для чтения
     $r=ldap_bind($ds);
+    
+    $dn        = 'ou=addressbook,dc=mail,dc=gorodavto,dc=com';
+    $filter    = '(|(telephonenumber=*))';
+    $justthese = array('ou', 'sn', 'cn', 'givenname', 'telephonenumber', 'title', 'mail');    
 
-    // ООО Альянс
-    // Аттрибуты поиска, подразделение alians-kmv, наличие аттрибута телефон (telephonenumber)
-//    $alians=ldap_search($ds, "ou=alians-kmv.ru,ou=addressbook,dc=mail,dc=gorodavto,dc=com", "telephonenumber=*");
-    $alians=ldap_search($ds, "ou=addressbook,dc=mail,dc=gorodavto,dc=com", "telephonenumber=*");
-
-    // Сортировка, критерий отдел (в данном представлении ldap-аттрибут "ou")
-//    ldap_get_entries($ds, $alians);
-
+    $alians=ldap_search($ds, $dn, $filter, $justthese);
+    
     // Получение записей, согласно заданным выше критериям поиска и сортировки
     $alianskmv = ldap_get_entries($ds, $alians);
 
@@ -48,40 +44,44 @@ if ($ds) {
     // Нумерация, начальное значение
     $row_alians = 1;
     
-//    print_r($alianskmv);
-
     // Таблица, заголовки таблицы
-//    echo '<table class="table table-striped">';
-//    echo '<thead>';
-//    echo '<th><b>№</b></span></th>';
-//    echo '<th><b> Ф.И.О </b></span></th>';
-//    echo '<th><b> Отдел </b></span></th>';
-////    echo '<th><b> Должность </b></span></th>';
-//    echo '<th><b> Рабочий телефон </b></span></th>';
-//    echo '</tr>';
-//    echo '</thead>';
-//    echo '<tbody>';     
-//
-//    // Для каждой, возвращенной записи отображать порядковый номер, Ф.И.О., Отдел, Должность, Рабочий телефон
-//    for ($i=0; $i<$alianskmv["count"]; $i++) {
-//        $rows = $row_alians++;
-//
-//        echo '<tr>';
-//        
-//        echo '<td>' . $rows . '.</td>';
-//        echo '<td>' . $alianskmv[$i]["cn"][0] . '</td>';
-//        echo '<td>' . $alianskmv[$i]["ou"][0] . '</td>';
-////        echo '<td>' . $alianskmv[$i]["title"][0] . '</td>';
-//        echo '<td>' . $alianskmv[$i]["telephonenumber"][0] . '</td>';        
-//        echo '</tr>';
-//
-//    }
-//
-//    echo '</tbody>';
-//    echo '</table>';    
+    echo '<table class="table table-striped">';
+    echo '<thead>';
+    echo '<th><b>№</b></span></th>';
+    echo '<th><b> Ф.И.О </b></span></th>';
+    echo '<th><b> Отдел </b></span></th>';
+    echo '<th><b> Должность </b></span></th>';
+    echo '<th><b> Рабочий телефон </b></span></th>';
+    echo '</tr>';
+    echo '</thead>';
+    echo '<tbody>';     
+
+    // Для каждой, возвращенной записи отображать порядковый номер, Ф.И.О., Отдел, Должность, Рабочий телефон
+    for ($i=0; $i<$alianskmv["count"]; $i++) {
+        $rows = $row_alians++;
+
+        echo '<tr>';
+        
+        echo '<td>' . $rows . '.</td>';
+        echo '<td>' . $alianskmv[$i]["cn"][0] . '</td>';
+        if(isset($alianskmv[$i]["ou"][0])){
+            echo '<td>' . $alianskmv[$i]["ou"][0] . '</td>';            
+        } 
+        if(isset($alianskmv[$i]["title"][0])){
+            echo '<td>' . $alianskmv[$i]["title"][0] . '</td>';            
+        }
+        if(isset($alianskmv[$i]["telephonenumber"][0])){
+            echo '<td>' . $alianskmv[$i]["telephonenumber"][0] . '</td>';        
+        }
+        echo '</tr>';
+
+    }
+
+    echo '</tbody>';
+    echo '</table>';    
 
     // Подчеркивание под таблицей
-//    echo "<hr />";
-        
+    echo "<hr />";    
+    
     ldap_close($ds);    
 }

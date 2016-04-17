0c5680fcb32da4e70236d416a8a21fb90fb8e4c1
Maxim Ishchenko <maxim.ishchenko@gmail.com>
2016-04-17 22:47:31 +0300

    Changed Alliance Creditcalendar buttonmenu

--- modules/alliance/views/creditcalendar/_buttonmenu.php
+++ modules/alliance/views/creditcalendar/_buttonmenu.php
@@ -19,7 +19,6 @@ $this->registerJs($upd, View::POS_END);
 <p style="text-align: right">
         <?= Html::a(FA::icon('plus') . ' ' . Module::t('module', 'CREDITCALENDAR_EVENT'), ['create?is_task=0'], ['class' => 'btn btn-success btn-sm', 'id' => 'refreshButton']) ?>
         <?= Html::a(FA::icon('plus') . ' ' . Module::t('module', 'CREDITCALENDAR_TASK'), ['create?is_task=1'], ['class' => 'btn btn-info btn-sm', 'id' => 'refreshButton']) ?>
-        <?php // Html::a(FA::icon('refresh') . ' ' . Module::t('module', 'CREDITCALENDAR_REFRESH'), ['calendar'], ['class' => 'btn btn-primary btn-sm', 'id' => 'refreshButton']) ?>
         <?php 
             if(Yii::$app->controller->action->id == 'index')
             {

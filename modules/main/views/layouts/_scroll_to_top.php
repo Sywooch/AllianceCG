e32bdf6161198d39c8e28caf0f1544ce08e2882f
Maxim Ishchenko <maxim.ishchenko@gmail.com>
2016-05-28 15:33:03 +0300

    small changes css

--- modules/main/views/layouts/_scroll_to_top.php
+++ modules/main/views/layouts/_scroll_to_top.php
@@ -1,5 +1,25 @@
 <?php 
-   $this->registerCssFile('@web/css/scroll-top.css', ['depends' => ['app\assets\AppAsset']]);  
+
+	use yii\helpers\Html;
+
+   	$this->registerCssFile('@web/css/scroll-top.css', ['depends' => ['app\assets\AppAsset']]);  
 ?>
 
 <?= \bluezed\scrollTop\ScrollTop::widget() ?>
+
+<script type="text/javascript">
+
+	var t;
+	function up() {
+		var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
+		if(top > 0) {
+			window.scrollBy(0,-100);
+			t = setTimeout('up()',10);
+		}
+		else clearTimeout(t);
+	return false;
+	}
+
+</script>
+
+<!-- <a href="#" onclick="return up()">наверх</a> -->

e7dc4b9553ee25f6cffc543e77650714a46d9a89
Maxim Ishchenko <maxim.ishchenko@gmail.com>
2016-06-10 14:05:39 +0300

    References Default Page Panels

--- modules/references/views/default/index.php
+++ modules/references/views/default/index.php
@@ -29,7 +29,275 @@
             </div>
         </div>
         <div class="panel-footer">
-            Panel Footer
+            <div class="row panel-footer-row">
+                <div class="col-xs-9">
+                    REFLINK
+                </div>
+                <div class="col-xs-3 text-right">
+                    <i class="fa fa-arrow-right"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="col-lg-3">
+    <div class="panel panel-primary">
+        <!-- <div class="panel-heading">
+            Panel with panel-primary class
+        </div> -->
+        <div class="panel-body panel-blue">
+            <div class="row panel-row">
+                <div class="col-xs-3">
+                    <i class="fa fa-comment fa-5x fa-inverse"></i>
+                </div>
+                <div class="col-xs-9 text-right">
+                    <h1>26</h1>
+                    <h4>Comments</h4>
+                </div>
+            </div>
+        </div>
+        <div class="panel-footer">
+            <div class="row panel-footer-row">
+                <div class="col-xs-9">
+                    REFLINK
+                </div>
+                <div class="col-xs-3 text-right">
+                    <i class="fa fa-arrow-right"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="col-lg-3">
+    <div class="panel panel-primary">
+        <!-- <div class="panel-heading">
+            Panel with panel-primary class
+        </div> -->
+        <div class="panel-body panel-blue">
+            <div class="row panel-row">
+                <div class="col-xs-3">
+                    <i class="fa fa-comment fa-5x fa-inverse"></i>
+                </div>
+                <div class="col-xs-9 text-right">
+                    <h1>26</h1>
+                    <h4>Comments</h4>
+                </div>
+            </div>
+        </div>
+        <div class="panel-footer">
+            <div class="row panel-footer-row">
+                <div class="col-xs-9">
+                    REFLINK
+                </div>
+                <div class="col-xs-3 text-right">
+                    <i class="fa fa-arrow-right"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="col-lg-3">
+    <div class="panel panel-primary">
+        <!-- <div class="panel-heading">
+            Panel with panel-primary class
+        </div> -->
+        <div class="panel-body panel-blue">
+            <div class="row panel-row">
+                <div class="col-xs-3">
+                    <i class="fa fa-comment fa-5x fa-inverse"></i>
+                </div>
+                <div class="col-xs-9 text-right">
+                    <h1>26</h1>
+                    <h4>Comments</h4>
+                </div>
+            </div>
+        </div>
+        <div class="panel-footer">
+            <div class="row panel-footer-row">
+                <div class="col-xs-9">
+                    REFLINK
+                </div>
+                <div class="col-xs-3 text-right">
+                    <i class="fa fa-arrow-right"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="col-lg-3">
+    <div class="panel panel-primary">
+        <!-- <div class="panel-heading">
+            Panel with panel-primary class
+        </div> -->
+        <div class="panel-body panel-blue">
+            <div class="row panel-row">
+                <div class="col-xs-3">
+                    <i class="fa fa-comment fa-5x fa-inverse"></i>
+                </div>
+                <div class="col-xs-9 text-right">
+                    <h1>26</h1>
+                    <h4>Comments</h4>
+                </div>
+            </div>
+        </div>
+        <div class="panel-footer">
+            <div class="row panel-footer-row">
+                <div class="col-xs-9">
+                    REFLINK
+                </div>
+                <div class="col-xs-3 text-right">
+                    <i class="fa fa-arrow-right"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="col-lg-3">
+    <div class="panel panel-primary">
+        <!-- <div class="panel-heading">
+            Panel with panel-primary class
+        </div> -->
+        <div class="panel-body panel-blue">
+            <div class="row panel-row">
+                <div class="col-xs-3">
+                    <i class="fa fa-comment fa-5x fa-inverse"></i>
+                </div>
+                <div class="col-xs-9 text-right">
+                    <h1>26</h1>
+                    <h4>Comments</h4>
+                </div>
+            </div>
+        </div>
+        <div class="panel-footer">
+            <div class="row panel-footer-row">
+                <div class="col-xs-9">
+                    REFLINK
+                </div>
+                <div class="col-xs-3 text-right">
+                    <i class="fa fa-arrow-right"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="col-lg-3">
+    <div class="panel panel-primary">
+        <!-- <div class="panel-heading">
+            Panel with panel-primary class
+        </div> -->
+        <div class="panel-body panel-blue">
+            <div class="row panel-row">
+                <div class="col-xs-3">
+                    <i class="fa fa-comment fa-5x fa-inverse"></i>
+                </div>
+                <div class="col-xs-9 text-right">
+                    <h1>26</h1>
+                    <h4>Comments</h4>
+                </div>
+            </div>
+        </div>
+        <div class="panel-footer">
+            <div class="row panel-footer-row">
+                <div class="col-xs-9">
+                    REFLINK
+                </div>
+                <div class="col-xs-3 text-right">
+                    <i class="fa fa-arrow-right"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="col-lg-3">
+    <div class="panel panel-primary">
+        <!-- <div class="panel-heading">
+            Panel with panel-primary class
+        </div> -->
+        <div class="panel-body panel-blue">
+            <div class="row panel-row">
+                <div class="col-xs-3">
+                    <i class="fa fa-comment fa-5x fa-inverse"></i>
+                </div>
+                <div class="col-xs-9 text-right">
+                    <h1>26</h1>
+                    <h4>Comments</h4>
+                </div>
+            </div>
+        </div>
+        <div class="panel-footer">
+            <div class="row panel-footer-row">
+                <div class="col-xs-9">
+                    REFLINK
+                </div>
+                <div class="col-xs-3 text-right">
+                    <i class="fa fa-arrow-right"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="col-lg-3">
+    <div class="panel panel-primary">
+        <!-- <div class="panel-heading">
+            Panel with panel-primary class
+        </div> -->
+        <div class="panel-body panel-blue">
+            <div class="row panel-row">
+                <div class="col-xs-3">
+                    <i class="fa fa-comment fa-5x fa-inverse"></i>
+                </div>
+                <div class="col-xs-9 text-right">
+                    <h1>26</h1>
+                    <h4>Comments</h4>
+                </div>
+            </div>
+        </div>
+        <div class="panel-footer">
+            <div class="row panel-footer-row">
+                <div class="col-xs-9">
+                    REFLINK
+                </div>
+                <div class="col-xs-3 text-right">
+                    <i class="fa fa-arrow-right"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="col-lg-3">
+    <div class="panel panel-primary">
+        <!-- <div class="panel-heading">
+            Panel with panel-primary class
+        </div> -->
+        <div class="panel-body panel-blue">
+            <div class="row panel-row">
+                <div class="col-xs-3">
+                    <i class="fa fa-comment fa-5x fa-inverse"></i>
+                </div>
+                <div class="col-xs-9 text-right">
+                    <h1>26</h1>
+                    <h4>Comments</h4>
+                </div>
+            </div>
+        </div>
+        <div class="panel-footer">
+            <div class="row panel-footer-row">
+                <div class="col-xs-9">
+                    REFLINK
+                </div>
+                <div class="col-xs-3 text-right">
+                    <i class="fa fa-arrow-right"></i>
+                </div>
+            </div>
         </div>
     </div>
 </div>
